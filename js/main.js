5b378f8c40f56a7301b96b0991bec077e0f738de
Ozielc4 <ozielc4@gmail.com>
2023-04-20 23:01:50 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+form.onsubmit = function(event) {
+   event.preventDefault()
+   const weight = inputWeight.value
+   const height = inputHeight.value
+
+   console.log(weight, height)
+}
